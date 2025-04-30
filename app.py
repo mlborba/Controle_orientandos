89f71b46e227965fa1e4699cd64818587c9a4688
mlborba <borbauniville@gmail.com>
2025-04-29 21:19:17 -0300

    Update app.py
    Aumentar tamanho da coluna password_hash para 256

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ login_manager.login_view = 'login'
 class Usuario(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(256), nullable=False) # Aumentado para 256
     nome = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     
